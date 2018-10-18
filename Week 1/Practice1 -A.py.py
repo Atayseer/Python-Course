d68693eac563b7862867858b379c930c5dd90e7a
Atayseer <44100907+Atayseer@users.noreply.github.com>
2018-10-18 20:40:29 +0300

    Add files via upload

---
+++ Week 1/Practice1 -A.py.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[53]:
+
+
+name = "tayseer"
+num1 = 10
+num2 = 7
+print(f"hi, my name is {name}")
+print(10 + 7)
+print(10 - 7)
+print(10/ 7)
+print(10 * 7)
+print(10 % 7)
+print(10 ** 7)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
