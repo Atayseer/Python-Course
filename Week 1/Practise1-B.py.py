4b99e2a624c1b3b4dc31bc5f45e6d5332326c888
Atayseer <44100907+Atayseer@users.noreply.github.com>
2018-10-18 23:05:04 +0300

    Add files via upload

---
+++ Week 1/Practise1-B.py.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+D) the best data type will be NUMERIC, because numeric data include all typesof numbers, and its the only data type that can be used in numeric calculations.
+
+
+# In[18]:
+
+
+x = float(input("value of x:"))
+if x < -100 : 
+    return -x
+
+else -100 <= x and x <= −25 :
+    return x**4
+elif −25 < 𝑥 and x <= 0 : 
+    return 3 ∗ 𝑥**2 − 1
+elif  0 < 𝑥  and x <= 100 : 
+    retur
+    n 3.14/2*x + 3**x
+elif  𝑥 > 100 : 
+    
+    return x 
+print(result)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
